c44811bea2319a3a076acabed276232234f280ab
caioau012 <caio_hudak@hotmail.com>
2025-10-17 22:23:38 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -37,12 +37,12 @@ public class Pawn extends ChessPiece{
 			}
 		}
 		else {
-			p.setValues(position.getRow() +1,  position.getColumn());
+			p.setValues(position.getRow() + 1,  position.getColumn());
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() +2,  position.getColumn());
-			Position p2 = new Position(position.getRow() +1,  position.getColumn());
+			p.setValues(position.getRow() + 2,  position.getColumn());
+			Position p2 = new Position(position.getRow() + 1,  position.getColumn());
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
